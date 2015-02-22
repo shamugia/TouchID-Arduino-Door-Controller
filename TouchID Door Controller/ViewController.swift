6256eaf662490d1fa03b0a8c9691cc68e36b870f
George Shamugia <George@Georges-MacBook-Pro.local>
2015-02-23 00:00:36 +0400

    Initial Commit

---
+++ TouchID Door Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TouchID Door Controller
+//
+//  Created by George Shamugia on 23/02/2015.
+//  Copyright (c) 2015 George Shamugia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
